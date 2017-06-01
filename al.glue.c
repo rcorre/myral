f8ae81c9ea7e9860baa2ee2e3dca166542a5940d
Ryan Roden-Corrent <ryan@rcorre.net>
2017-05-31 22:53:48 -0400

    Rename to al.
    Naming parallels allegro:
    
    al_* -> al.*

--- al.glue.c
+++ al.glue.c
@@ -1,13 +1,13 @@
 /* LIBS: c allegro */
 #include <allegro5/allegro.h>
 
-int myral$al_init()
+int al$init()
 {
     return al_init();
 }
 
 
-int myral$al_create_display(int w, int h)
+ALLEGRO_DISPLAY* al$create_display(int w, int h)
 {
     return al_create_display(w, h);
 }
