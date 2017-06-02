533f7f4ba3c013591975c41963d1e34eae431259
Ryan Roden-Corrent <ryan@rcorre.net>
2017-06-02 08:11:21 -0400

    Separate main module from display module

--- al.glue.c
+++ al.glue.c
@@ -6,8 +6,12 @@ int al$init()
     return al_init();
 }
 
-
 ALLEGRO_DISPLAY* al$create_display(int w, int h)
 {
     return al_create_display(w, h);
+}
+
+void al$destroy_display(ALLEGRO_DISPLAY *disp)
+{
+    al_destroy_display(disp);
 }
